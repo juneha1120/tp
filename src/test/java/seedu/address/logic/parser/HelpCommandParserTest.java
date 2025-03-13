d9a0347267707e84e50b6054a1c7b158a05f01fc
ChenFan <e1129819@u.nus.edu>
2025-03-14 03:23:59 +0800

    feat: add more tests on HelpCommand and HelpCommandParser

--- src/test/java/seedu/address/logic/parser/HelpCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/HelpCommandParserTest.java
@@ -7,7 +7,9 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class HelpCommandParserTest {
@@ -19,6 +21,14 @@ public class HelpCommandParserTest {
         String args = AddCommand.COMMAND_WORD;
         HelpCommand expectedCommand = new HelpCommand(AddCommand.COMMAND_WORD, AddCommand.MESSAGE_USAGE,
                 "Adds a new contact to the database with optional details.");
+        assertEquals(expectedCommand, parser.parse(args));
+    }
+
+    @Test
+    public void parse_validListCommand_returnsHelpCommand() throws ParseException {
+        String args = ListCommand.COMMAND_WORD;
+        HelpCommand expectedCommand = new HelpCommand(ListCommand.COMMAND_WORD, ListCommand.COMMAND_WORD,
+                "Lists out the contact in the database with optional category.");
         assertEquals(expectedCommand, parser.parse(args));
     }
 
@@ -36,4 +46,10 @@ public class HelpCommandParserTest {
         assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE), exception.getMessage());
     }
 
+    @Test
+    public void parse_whitespaceArgs_returnsDefaultHelpCommand() throws ParseException {
+        String args = "   ";
+        HelpCommand expectedCommand = new HelpCommand();
+        assertEquals(expectedCommand, parser.parse(args));
+    }
 }
