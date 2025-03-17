9b73471dd39d5e95da74d02da60f543a6213504c
ChenFan <e1129819@u.nus.edu>
2025-03-17 17:21:52 +0800

    fix: remove unused import

--- src/main/java/seedu/address/logic/parser/HelpCommandParser.java
+++ src/main/java/seedu/address/logic/parser/HelpCommandParser.java
@@ -2,9 +2,6 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.HelpCommand;
