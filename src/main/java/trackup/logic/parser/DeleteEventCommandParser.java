4617828ca75144ee39200777d47e12023a2d162d
Ha Jiwoon <juneha1120@gmail.com>
2025-03-24 17:25:19 +0800

    fix: Edit DeleteEventCommandParser's parse method to correctly handle

--- src/main/java/trackup/logic/parser/DeleteEventCommandParser.java
+++ src/main/java/trackup/logic/parser/DeleteEventCommandParser.java
@@ -1,5 +1,6 @@
 package trackup.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static trackup.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static trackup.logic.parser.CliSyntax.PREFIX_EVENT_CONTACT;
 import static trackup.logic.parser.CliSyntax.PREFIX_EVENT_END;
@@ -7,6 +8,7 @@ import static trackup.logic.parser.CliSyntax.PREFIX_EVENT_START;
 import static trackup.logic.parser.CliSyntax.PREFIX_EVENT_TITLE;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 import trackup.commons.core.index.Index;
@@ -23,18 +25,34 @@ public class DeleteEventCommandParser implements Parser<DeleteEventCommand> {
      * @throws ParseException if the user input does not conform to the expected format.
      */
     public DeleteEventCommand parse(String args) throws ParseException {
+        requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
                 PREFIX_EVENT_TITLE, PREFIX_EVENT_START, PREFIX_EVENT_END, PREFIX_EVENT_CONTACT);
 
-        String titleKeyword = argMultimap.getValue(PREFIX_EVENT_TITLE).orElse(null);
-        LocalDateTime startTime = ParserUtil.parseEventTime(argMultimap.getValue(PREFIX_EVENT_START).orElse(null));
-        LocalDateTime endTime = ParserUtil.parseEventTime(argMultimap.getValue(PREFIX_EVENT_END).orElse(null));
-        Set<Index> contactIndexes = ParserUtil.parseContacts(argMultimap.getAllValues(PREFIX_EVENT_CONTACT));
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_EVENT_TITLE, PREFIX_EVENT_START, PREFIX_EVENT_END);
 
-        if (titleKeyword == null && startTime == null && endTime == null && contactIndexes.isEmpty()) {
+        String partialTitle = null;
+        LocalDateTime startDateTime = null;
+        LocalDateTime endDateTime = null;
+        Set<Index> contactIndexes = new HashSet<>();
+
+        if (argMultimap.getValue(PREFIX_EVENT_TITLE).isPresent()) {
+            partialTitle = argMultimap.getValue(PREFIX_EVENT_TITLE).orElse(null);
+        }
+        if (argMultimap.getValue(PREFIX_EVENT_START).isPresent()) {
+            startDateTime = ParserUtil.parseEventTime(argMultimap.getValue(PREFIX_EVENT_START).orElse(null));
+        }
+        if (argMultimap.getValue(PREFIX_EVENT_END).isPresent()) {
+            endDateTime = ParserUtil.parseEventTime(argMultimap.getValue(PREFIX_EVENT_END).orElse(null));
+        }
+        if (!argMultimap.getAllValues(PREFIX_EVENT_CONTACT).isEmpty()) {
+            contactIndexes = ParserUtil.parseContacts(argMultimap.getAllValues(PREFIX_EVENT_CONTACT));
+        }
+
+        if (partialTitle == null && startDateTime == null && endDateTime == null && contactIndexes.isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteEventCommand.MESSAGE_USAGE));
         }
 
-        return new DeleteEventCommand(titleKeyword, startTime, endTime, contactIndexes);
+        return new DeleteEventCommand(partialTitle, startDateTime, endDateTime, contactIndexes);
     }
 }
