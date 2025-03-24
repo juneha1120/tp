2d779d35561e5658086b185bdb0b5846a2667504
Ha Jiwoon <juneha1120@gmail.com>
2025-03-24 17:31:49 +0800

    feat: Add CLI syntax relevant to Event

--- src/main/java/trackup/logic/parser/CliSyntax.java
+++ src/main/java/trackup/logic/parser/CliSyntax.java
@@ -12,4 +12,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("-a ");
     public static final Prefix PREFIX_TAG = new Prefix("-t ");
     public static final Prefix PREFIX_CATEGORY = new Prefix("-c ");
+    public static final Prefix PREFIX_EVENT_TITLE = new Prefix("-et ");
+    public static final Prefix PREFIX_EVENT_START = new Prefix("-es ");
+    public static final Prefix PREFIX_EVENT_END = new Prefix("-ee ");
+    public static final Prefix PREFIX_EVENT_CONTACT = new Prefix("-ec ");
 }
