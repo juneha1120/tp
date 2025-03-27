296cffc790ed6a2bf38d5223d1a97d07b7c5a693
juneha1120 <111293167+juneha1120@users.noreply.github.com>
2025-03-28 00:11:32 +0800

    style: Fix checkstyle error

--- src/main/java/trackup/model/AddressBook.java
+++ src/main/java/trackup/model/AddressBook.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import trackup.commons.util.ToStringBuilder;
 import trackup.model.event.Event;
