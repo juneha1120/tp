a2d0aaddf5d87e432fc82dbf42758874ecdb26e2
Arshin Sikka <e1186295@u.nus.edu>
2025-04-01 02:32:58 +0800

    test(add): remove outdated generic missing field test after switching to specific error messages

--- src/test/java/trackup/logic/parser/AddCommandParserTest.java
+++ src/test/java/trackup/logic/parser/AddCommandParserTest.java
@@ -148,31 +148,6 @@ public class AddCommandParserTest {
     }
 
     @Test
-    public void parse_compulsoryFieldMissing_failure() {
-        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE);
-
-        // missing name prefix
-        assertParseFailure(parser, VALID_NAME_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
-
-        // missing phone prefix
-        assertParseFailure(parser, NAME_DESC_BOB + VALID_PHONE_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
-
-        // missing email prefix
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + VALID_EMAIL_BOB + ADDRESS_DESC_BOB,
-                expectedMessage);
-
-        // missing address prefix
-        assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + VALID_ADDRESS_BOB,
-                expectedMessage);
-
-        // all prefixes missing
-        assertParseFailure(parser, VALID_NAME_BOB + VALID_PHONE_BOB + VALID_EMAIL_BOB + VALID_ADDRESS_BOB,
-                expectedMessage);
-    }
-
-    @Test
     public void parse_invalidValue_failure() {
         // invalid name
         assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
@@ -231,6 +206,5 @@ public class AddCommandParserTest {
         String input = NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY;
         assertParseFailure(parser, input, "Missing required field: Address (-a)");
     }
-
 
 }
