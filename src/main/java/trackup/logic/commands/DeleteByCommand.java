41ffa42d1f074ab1743488a7efb0669fd7ccc029
Arshin Sikka <e1186295@u.nus.edu>
2025-04-01 01:31:44 +0800

    fix(deleteby): update multiple matches error message to be more user-friendly

--- src/main/java/trackup/logic/commands/DeleteByCommand.java
+++ src/main/java/trackup/logic/commands/DeleteByCommand.java
@@ -47,7 +47,8 @@ public class DeleteByCommand extends Command {
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
     public static final String MESSAGE_NO_PERSON_TO_DELETE = "No Person Matches Criteria: %1$s";
-    public static final String MESSAGE_MULTIPLE_PEOPLE_TO_DELETE = "Multiple People Matches Criteria: %1$s";
+    public static final String MESSAGE_MULTIPLE_PEOPLE_TO_DELETE =
+            "Multiple contacts match the provided attributes. Please refine your input to uniquely identify a contact.";
     public static final String MESSAGE_NO_CRITERIA_SPECIFIED = "At least one field to edit must be provided.";
 
     private final Optional<Name> deleteByName;
@@ -130,7 +131,7 @@ public class DeleteByCommand extends Command {
             return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
         } else {
             model.updateFilteredPersonList(predicate);
-            return new CommandResult(String.format(MESSAGE_MULTIPLE_PEOPLE_TO_DELETE, this.toString()));
+            return new CommandResult(MESSAGE_MULTIPLE_PEOPLE_TO_DELETE);
         }
     }
 
