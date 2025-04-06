3b5ac5d9b41ac38f538b8dd565f3bdd88cf75335
Arshin Sikka <e1186295@u.nus.edu>
2025-04-06 19:01:02 +0800

    Fix Checkstyle error: add space after semicolon in ListCommandParserTest

--- src/test/java/trackup/logic/parser/ListCommandParserTest.java
+++ src/test/java/trackup/logic/parser/ListCommandParserTest.java
@@ -1,2 +1,30 @@
-package trackup.logic.parser;public class ListCommandParserTest {
+package trackup.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import trackup.logic.commands.ListCommand;
+import trackup.logic.parser.exceptions.ParseException;
+
+public class ListCommandParserTest {
+
+    private final ListCommandParser parser = new ListCommandParser();
+
+    @Test
+    public void parse_validCategory_success() throws Exception {
+        assertTrue(parser.parse("Client") instanceof ListCommand);
+    }
+
+    @Test
+    public void parse_invalidCategory_throwsParseException() {
+        String invalidCategory = "Aliens";
+        assertThrows(ParseException.class, () -> parser.parse(invalidCategory));
+    }
+
+    @Test
+    public void parse_emptyArgs_success() throws Exception {
+        assertTrue(parser.parse("") instanceof ListCommand);
+    }
 }
