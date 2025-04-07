8edd6ae559d224256605f0b538b8498e6cd31fee
caando <zhangjikun907475@gmail.com>
2025-04-07 23:28:24 +0800

    refactor: Rename instances of addressbook to trackup

--- src/test/java/trackup/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/trackup/storage/JsonUserPrefsStorageTest.java
@@ -74,7 +74,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100, new Visibility()));
-        userPrefs.setAddressBookFilePath(Paths.get("addressbook.json"));
+        userPrefs.setAddressBookFilePath(Paths.get("trackup.json"));
         return userPrefs;
     }
 
